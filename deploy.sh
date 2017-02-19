7af268d748f63900536d5c79134328ce2157493a
Christophe Benz <christophe.benz@gmail.com>
2017-02-19 12:01:08 +0100

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
+REV=$(git rev-parse HEAD)
+
 rm -rf dist
-elm-make src/Main.elm --warn --output=dist/main.js
-cp index.gh-pages.html dist/index.html
-cd dist
+rm -rf elm-stuff/build-artifacts # To display all the warnings, do not keep old compilation results.
+
+elm make src/Main.elm --warn --output=dist/main.js
+
+cp index.html dist
+sed -i -- 's~/_compile/src/Main.elm~main.js~' dist/index.html
+sed -i -- 's~runElmProgram~Elm.Main.fullscreen~' dist/index.html
+
+echo "Press Enter to deploy or Ctrl-C to abort (to fix warnings, to test with 'static dist' server)..."
+read
+
+pushd dist
 git init
 git add .
-git commit -m "Deploy to Github Pages"
+git commit -m "Build application from commit $REV"
 git push --force git@github.com:openchordcharts/chart-editor.git master:gh-pages
+popd
+
+# Refresh the gh-pages branch in particular.
+git fetch
