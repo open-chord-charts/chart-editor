f55784e0489e385dc46752d9b46bdc234bea54ef
Christophe Benz <christophe.benz@gmail.com>
2017-03-26 00:43:03 +0100

    Tweak package.json scripts

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-npm run build
-
-if [ $? != 0 ]; then
-    echo "Build failed, abandon deploy."
-    exit 1
-fi
-
 cp index.html dist
 sed -i -- 's~/_compile/src/Main.elm~main.js~' dist/index.html
 sed -i -- 's~runElmProgram~Elm.Main.fullscreen~' dist/index.html
