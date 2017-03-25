a4b95c9623c084505255ab2c2ea874c93315665c
Christophe Benz <christophe.benz@gmail.com>
2017-03-26 00:21:20 +0100

    Use package.json for scripts

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 
-REV=$(git rev-parse HEAD)
+npm run build
 
-rm -rf dist
-rm -rf elm-stuff/build-artifacts # To display all the warnings, do not keep old compilation results.
-
-elm make src/Main.elm --warn --output=dist/main.js
+if [ $? != 0 ]; then
+    echo "Build failed, abandon deploy."
+    exit 1
+fi
 
 cp index.html dist
 sed -i -- 's~/_compile/src/Main.elm~main.js~' dist/index.html
 sed -i -- 's~runElmProgram~Elm.Main.fullscreen~' dist/index.html
 
-echo "Press Enter to deploy or Ctrl-C to abort (to fix warnings, to test with 'static dist' server)..."
-read
-
-pushd dist
-git init
-git add .
-git commit -m "Build application from commit $REV"
-git push --force git@github.com:open-chords-charts/chart-editor.git master:gh-pages
-popd
+gh-pages --dist dist
 
 # Refresh the gh-pages branch in particular.
 git fetch
